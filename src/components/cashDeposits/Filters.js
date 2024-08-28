5998f0bb06da5832db985163f9f7bc37835edc0f
Praphullag1002 <praphulla.g@comakeit.com>
2024-08-28 15:13:05 +0530

    fixed reports

--- src/components/cashDeposits/Filters.js
+++ src/components/cashDeposits/Filters.js
@@ -30,10 +30,9 @@ const Filters = ({ isFilteredData, setisFilteredData, setIsFind }) => {
 
   const handleExport = async (e) => {
     e.preventDefault();
-    console.log("Filters before generating export URL:", filters);
     const params = new URLSearchParams();
 
-    params.append("report[additional_data][search]", filters.search || "");
+    params.append("report[kind]", "cash_deposits");
     params.append(
       "report[additional_data][created_at_from]",
       filters.createdFrom || ""
@@ -50,7 +49,7 @@ const Filters = ({ isFilteredData, setisFilteredData, setIsFind }) => {
       "report[additional_data][deposit_date_on_until]",
       filters.depositDateUntil || ""
     );
-    params.append("report[kind]", "cash_deposits");
+    params.append("report[additional_data][search]", filters.search || "");
 
     filters.marketId.forEach((id) => {
       params.append("report[additional_data][market_ids][]", id);
@@ -68,13 +67,10 @@ const Filters = ({ isFilteredData, setisFilteredData, setIsFind }) => {
     console.log("Generated URL:", url);
 
     try {
-      const response = await axios.get(url);
-      console.log("Response data:", response.data);
-      console.log(response.status,"statussss")
+      const response = await axios.post(url);
       if (response.status === 200) {
         window.location.href = "http://manage.lvh.me:5000/reports";
       }
-      console.log("Export data set:", response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
