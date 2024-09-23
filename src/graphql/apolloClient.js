fc83e636c23660ca65e1167d5e4324a82d3052ce
Harika2225 <harikasree2k@gmail.com>
2024-09-23 16:17:07 +0530

    passing hasura secret to headers

--- src/graphql/apolloClient.js
+++ src/graphql/apolloClient.js
@@ -1,9 +1,24 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_HASURA_GRAPHQL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  console.log('GraphQL Endpoint:', process.env.REACT_APP_HASURA_GRAPHQL);
+  console.log('Admin Secret:', process.env.REACT_APP_HASURA_ADMIN_SECRET);
+
+  return {
+    headers: {
+      ...headers,
+      'x-hasura-admin-secret': process.env.REACT_APP_HASURA_ADMIN_SECRET,
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: process.env.REACT_APP_HASURA_GRAPHQL,
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
